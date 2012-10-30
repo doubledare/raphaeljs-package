3945af98a318e189d45e9118db4040ea8cbb9596
Joc <jocphone@gmail.com>
2012-10-30 06:36:17 +0000

    removed bounding array

--- package.js
+++ package.js
@@ -3,5 +3,5 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
-  api.add_files('lib/raphael/raphael-min.js', ['client']);
+  api.add_files('lib/raphael/raphael-min.js', 'client');
 });
